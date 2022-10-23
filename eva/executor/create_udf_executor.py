9eefd19aebcf6e633da16d66ec03f4f23949a1e6
Ubuntu <azureuser@pr1-vm.ran3ya3dvhxenjg1hcutw0p5cf.bx.internal.cloudapp.net>
2022-10-23 14:53:08 +0000

    get predictions from mnist udf and calculate metrics object

--- eva/executor/create_udf_executor.py
+++ eva/executor/create_udf_executor.py
@@ -20,7 +20,7 @@ from eva.models.storage.batch import Batch
 from eva.planner.create_udf_plan import CreateUDFPlan
 from eva.utils.generic_utils import path_to_class
 from eva.utils.logging_manager import logger
-
+from eva.utils.profiler import Profiler
 
 class CreateUDFExecutor(AbstractExecutor):
     def __init__(self, node: CreateUDFPlan):

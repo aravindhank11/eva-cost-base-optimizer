d848e913408d37cbeeadb6e7adf44c82b5b8fdbf
Netra Ghaisas <netraghaisas@gmail.com>
2022-10-24 15:52:32 -0400

    chore: Changes to parser for profiler sample

--- client.py
+++ client.py
@@ -11,6 +11,11 @@ cursor.execute("DROP UDF IF EXISTS FastRCNNObjectDetector")
 response = cursor.fetch_all()
 print(response)
 
+# Insert a profiler sample
+cursor.execute("CREATE PROFILER SAMPLE FOR TYPE 'Classification' SAMPLE '' VALIDATION ''")
+response = cursor.fetch_all()
+print(response)
+
 # Re-Create the UDF
 cmd = "CREATE UDF IF NOT EXISTS FastRCNNObjectDetector INPUT  (frame NDARRAY UINT8(3, ANYDIM, ANYDIM)) OUTPUT (labels NDARRAY STR(ANYDIM), bboxes NDARRAY FLOAT32(ANYDIM, 4), scores NDARRAY FLOAT32(ANYDIM)) TYPE Classification IMPL 'eva/udfs/fastrcnn_object_detector.py';"
 cursor.execute(cmd)
